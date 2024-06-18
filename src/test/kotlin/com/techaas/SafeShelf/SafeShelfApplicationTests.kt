34d74e747f7b9ac69b05678a84ceb1d1fd80bf1b
emapfff <dvlemil_777@mail.ru>
2024-06-18 19:30:28 +0300

    Initialize backend

---
+++ src/test/kotlin/com/techaas/SafeShelf/SafeShelfApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.techaas.SafeShelf
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SafeShelfApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
