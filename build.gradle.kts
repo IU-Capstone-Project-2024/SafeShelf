0694d2542a211d83fe9897f885cb47af0280deae
Andrey Alexeev <jandr21@ya.ru>
2024-06-26 22:14:52 +0300

    Fix conflicts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("org.springframework.boot") version "3.3.0"
     id("io.spring.dependency-management") version "1.1.5"
+    id("org.asciidoctor.jvm.convert") version "3.3.2"
     kotlin("jvm") version "1.9.24"
     kotlin("plugin.spring") version "1.9.24"
     kotlin("plugin.allopen") version "1.9.24"
