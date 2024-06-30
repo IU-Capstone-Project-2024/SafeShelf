4dbf5f49f5ea23098bef729fdf40bcbd821fe822
jj228 <biathlon45@gmail.com>
2024-07-01 00:48:04 +0300

    Service additions

--- src/main/kotlin/com/techaas/services/AccountService.kt
+++ src/main/kotlin/com/techaas/services/AccountService.kt
@@ -1,9 +1,21 @@
 package com.techaas.services
 
+import com.techaas.domain.jpa.JpaUserService
+import com.techaas.dto.requests.LoginAccountRequest
+import com.techaas.exceptions.UserDoesntExistException
+import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
 
 @Service
 class AccountService(
+    val jpaUserService: JpaUserService
 
 ) {
+    @Transactional
+    fun userLogin(accountRequest: LoginAccountRequest) {
+        if (!jpaUserService.checkIfTheUserExists(accountRequest.login))
+        {
+            throw UserDoesntExistException("User is not registered yet")
+        }
+    }
 }
